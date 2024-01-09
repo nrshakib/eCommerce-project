bbf2ce0a966031e3ca4cf3145165a323b1bef195
NRS\nrsha <nrshakib007@gmail.com>
2024-01-10 02:16:44 +0600

    Updates in page design

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -35,24 +35,26 @@ const ProductDetails = (props) => {
           </div>
         </div>
         <div className="ml-5">
-          <p className="text-3xl mb-1">{product?.name}</p>
-          <div className="flex">
-            <p className="mr-2 text-xl font-semibold text-red-500 mb-1">
+          <p className="text-3xl mb-2">{product?.name}</p>
+          <div className="flex mt-2">
+            <p className="mr-2 text-2xl font-semibold text-red-500 mb-1">
               ${product?.new_price}
             </p>
-            <p className="text-slate-600 line-through mb-1">
+            <p className="text-slate-600 line-through mt-2 text-sm">
               ${product?.old_price}
             </p>
           </div>
-          <div className="flex text-yellow-500 mb-1">
-            <IoIosStar />
-            <IoIosStar />
-            <IoIosStar />
-            <IoIosStar />
-            <IoIosStarHalf />
+          <div className="flex mt-2 mb-2">
+            <div className="flex text-yellow-500 mb-2">
+              <IoIosStar />
+              <IoIosStar />
+              <IoIosStar />
+              <IoIosStar />
+              <IoIosStarHalf />
+            </div>
+            <p className="mb-1 text-sm ml-2">(122)</p>
           </div>
-          <p>(122)</p>
-          <p className="w-96 mb-2">
+          <p className="w-96 mb-5">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
             dolores sunt fugiat aspernatur libero commodi dolorem reiciendis eum
             possimus ipsum.
